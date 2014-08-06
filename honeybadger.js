77276d5c2a46d72f40a7f2c3d08d0a281139da10
Jico Baligod <jico@baligod.com>
2014-08-06 10:55:01 -0400

    build

--- honeybadger.js
+++ honeybadger.js
@@ -1106,9 +1106,6 @@ TraceKit.computeStackTrace = (function computeStackTraceWrapper() {
 if (!TraceKit.remoteFetching) {
   TraceKit.remoteFetching = true;
 }
-if (!TraceKit.collectWindowErrors) {
-  TraceKit.collectWindowErrors = true;
-}
 if (!TraceKit.linesOfContext || TraceKit.linesOfContext < 1) {
   // 5 lines before, the offending line, 5 lines after
   TraceKit.linesOfContext = 11;
